7c011447c2032437ab02b80483a52a2198e68a07
Bruno <brunonoriller@gmail.com>
2022-04-02 14:41:51 -0300

    big display grid

--- src/components/BoxesWrapper.jsx
+++ src/components/BoxesWrapper.jsx
@@ -1,5 +1,17 @@
 import './BoxesWrapper.css';
 
 export function BoxesWrapper() {
-  return <div className='wrapper'>boxes here</div>;
+  return (
+    <div className='wrapper'>
+      <div className='box main'>1</div>
+      <div className='box row3 col6'>2</div>
+      <div className='box row3 col3'>3</div>
+      <div className='box row3 col3'>4</div>
+      <div className='box five'>5</div>
+      <div className='box six'>6</div>
+      <div className='box seven'>7</div>
+      <div className='box eight'>8</div>
+      <div className='box nine'>9</div>
+    </div>
+  );
 }
